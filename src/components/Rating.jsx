739570ef0863151e9f587317ff26a341971ee7d3
antonio.junior_wipro <agcjunior@gmail.com>
2026-04-23 07:46:06 -0300

    First project

---
+++ src/components/Rating.jsx
@@ -0,0 +1,16 @@
+const Rating = () => {
+  return (
+    <div className="rating-container">
+      <h2>Rate your Experience</h2>
+      <div className="stars">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <span key={star} className="star">
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
